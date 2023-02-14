33e06b86a49ad2ad04d3877a26ca7e49c6707d9a
zxy <gcoordinate@gmail.com>
2023-02-14 19:46:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,12 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+const http = require('http');
 const PORT = process.env.PORT || 8081;
 
-app.get('/', async (req, res) => {
-  const username = req.query.username || 'myogeshchavan97';
-  try {
-    const result = await axios.get(
-      `https://api.github.com/users/${username}/repos`
-    );
-    const repos = result.data
-      .map((repo) => ({
-        name: repo.name,
-        url: repo.html_url,
-        description: repo.description,
-        stars: repo.stargazers_count
-      }))
-      .sort((a, b) => b.stars - a.stars);
-
-    res.send(repos);
-  } catch (error) {
-    res.status(400).send('Error while getting list of repositories');
-  }
+const server = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type':'text/plain'});
+  res.write("Hello World");
+  res.end();
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, function() {
   console.log(`server started on port ${PORT}`);
 });
